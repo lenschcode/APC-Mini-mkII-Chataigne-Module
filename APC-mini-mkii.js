96945b1188ca807646ba6b3b7b1d2d835e8075c2
lenschcode <danlennardhaensch@gmail.com>
2024-03-03 00:50:37 +0100

    v1.0.0

--- APC-mini-mkii.js
+++ APC-mini-mkii.js
@@ -12,7 +12,14 @@ var buttonNotes = [
 var sideButtonNotes = [112, 113, 114, 115, 116, 117, 118, 119];
 var bottomButtonNotes = [100, 101, 102, 103, 104, 105, 106, 107, 122];
 
+var noteValueObj = [];
+var noteColorObj = [];
+var notePulsingObj = [];
+var ccValueObj = [];
+
+var faderValueObj = [];
 var buttonValueObj = [];
+
 var colorParameterObj = [];
 var pulsingParameterObj = [];
 
@@ -23,123 +30,104 @@ var previousStep = -1;
 
 
 
-// ---- Midi Functions ----
+// ------ Feedback Functions ------
 
-function setMidiMode()
-{
-	local.sendSysex(0x00, 0x20, 0x29, 0x02, 0x0D, 0x0E, 0x01);
-	script.log("Set Midi Mode with Sysex Message");
+function calcMSBLSB(value) {
+    var MSB = (value & 0xFF00) >> 8;
+    var LSB = value & 0x00FF;
+    return [MSB, LSB];
 }
 
-
-function setButtonColorBrightness(id, color, brightness)
+function sendButtonColorBrightness(id, color, brightness)
 {
-	var noteStart = buttonNotes[id[0]][id[1]];
-	var noteEnd = noteStart;
+	var note = buttonNotes[id[0]][id[1]];
 
 					// newColor[1] = Math.round((color[1] * intensity) / 255);
 	var r = Math.round(((color[0] * 127) * brightness) / 255);
 	var g = Math.round(((color[1] * 127) * brightness) / 255);
 	var b = Math.round(((color[2] * 127) * brightness) / 255);
 
-	script.log("Set Color for note " + note + " to rgb:{"+r+","+g+","+b+"} Intensity: "+brightness);
-	local.sendSysex(0x47, 0x7F, 0x4F, 0x24, 0x00, 0x08, noteStart, noteEnd, r, r, g, g, b, b);
+	var rMSBLSB = calcMSBLSB(r);
+	var gMSBLSB = calcMSBLSB(g);
+	var bMSBLSB = calcMSBLSB(b);
+
+	// script.log("Set Color for note " + note + " to rgb:{"+r+","+g+","+b+"} Intensity: "+brightness);
+	local.sendSysex(0x47, 0x7F, 0x4F, 0x24, 0x00, 0x08, note, note, rMSBLSB[0], rMSBLSB[1], gMSBLSB[0], gMSBLSB[1], bMSBLSB[0], bMSBLSB[1]);
+	util.delayThreadMS(1);
 }
 
 
-function setButtonColor(id, color)
+function sendButtonColor(id, color)
 {
 	var note = buttonNotes[id[0]][id[1]];
 	var r = Math.round(color[0] * 127);
 	var g = Math.round(color[1] * 127);
 	var b = Math.round(color[2] * 127);
 
+	var rMSBLSB = calcMSBLSB(r);
+	var gMSBLSB = calcMSBLSB(g);
+	var bMSBLSB = calcMSBLSB(b);
+
 	script.log("Set Color for note " + note + " to rgb:{"+r+","+g+","+b+"}");
-	local.sendSysex(0x00, 0x20, 0x29, 0x02, 0x0D, 0x03, 0x03, note, r, g, b);
+	local.sendSysex(0x47, 0x7F, 0x4F, 0x24, 0x00, 0x08, note, note, rMSBLSB[0], rMSBLSB[1], gMSBLSB[0], gMSBLSB[1], bMSBLSB[0], bMSBLSB[1]);
+	util.delayThreadMS(1);
 }
 
-
-
-// ---- Helper Functions ----
-
-function getButtonForNote(note) {
-	for (var i = 0; i < 9; i++)
+function sendButton(side, index, state)
+{
+	if (side)
 	{
-		if (buttonNotes[i].contains(note))
-		{
-			return buttonValueObj[i][(buttonNotes[i].indexOf(note))];
-		}
+		var note = sideButtonNotes[index];
+		local.sendNoteOn(0, note, state ? 127 : 0);
 	}
-	return null;
-}
-
-
-function getFaderForNote(note) {
-	return faderNotes[i].indexOf(note)
-}
-
-function getColorParameterForNote(note) {
-	for (var i = 0; i < 9; i++)
+	else
 	{
-		if (buttonNotes[i].contains(note))
-		{
-			return colorParameterObj[i][(buttonNotes[i].indexOf(note))];
-		}
+		var note = bottomButtonNotes[index];
+		local.sendNoteOn(0, note, state ? 127 : 0);
 	}
-	return null;
 }
-
-
-function getPulsingParameterForNote(note) {
-	for (var i = 0; i < 9; i++)
-	{
-		if (buttonNotes[i].contains(note))
-		{
-			return pulsingParameterObj[i][(buttonNotes[i].indexOf(note))];
-		}
-	}
-	return null;
-}
-
-
-
 // ---- User Commands ----
 
 function resendColors()
 {
-	for(var i=0; i<9; i++)
+	for(var i = 0; i < 8; i++)
 	{
-		for(var j=0; j<9; j++)
+		for(var j = 0; j < 8; j++)
 		{
 			var color = colorParameterObj[i][j].get();
-			setButtonColor([i,j], color);
+			sendButtonColor([i,j], color);
 		}
 	}
 }
 
-
-function setColor(button, color)
+function resync()
 {
-	button.set(color);
-}
-
-
-function setColorByNote(note, color, pulsing)
-{
-	var colorObj = getColorParameterForNote(note);
-	var pulsingObj = getPulsingParameterForNote(note);
-
-	if (!!colorObj)
+	// Main Buttons
+	for (row = 0; row < 8; row++)
 	{
-		colorObj.set(color);
+		for (col = 0; col < 8; col ++)
+		{
+			var color = colorParameterObj[row][col].get();
+			var pulsing = pulsingParameterObj[row][col].get();
+
+			sendButtonColor([row, col], color);
+			util.delayThreadMS(1);
+		}
 	}
 
-	if (!!pulsingObj)
+	// Other Buttons
+	for (i = 0; i < 8; i++)
 	{
-		pulsingObj.set(pulsing);
-	}
+		// Side Buttons
+		var value = colorParameterObj[i][8].get();
+		sendButton(true, i, value);
+		util.delayThreadMS(1);
 
-	script.log("Set Button with Note "+note+" to color "); //TODO
+		// Bottom Buttons
+		var value = colorParameterObj[8][i].get();
+		sendButton(false, i, value);
+		util.delayThreadMS(1);
+	}
 }
 
 // ---- Module Common Functions ----
@@ -156,67 +144,153 @@ function init()
 		pulsingParameterObj[i] = [];
 	}
 
-	// Logo
-	colorParameterObj[0][8] = local.parameters.colors.logo.getChild("logo");
-	pulsingParameterObj[0][8] = local.parameters.pulsing.logo.getChild("logo");
-
-	// Top Buttons
-    for (var i = 0; i < 8; i++) 
-	{
-        buttonValueObj[0][i] = local.values.getChild("Top Buttons").getChild("topButton" + (i + 1) + "");
-        colorParameterObj[0][i] = local.parameters.colors.getChild("Top Buttons").getChild("topButton" + (i + 1) + "");
-        pulsingParameterObj[0][i] = local.parameters.pulsing.getChild("Top Buttons").getChild("topButton" + (i + 1) + "");
-    }
+	// Faders
+	for (var i = 0; i < 9; i++) {
+		faderValueObj[i] = local.values.faders.getChild("fader" + (i+1));
+		ccValueObj[faderNotes[i]] = local.values.faders.getChild("fader" + (i+1));
+	}
 
 	// Main Buttons
     for (var i = 0; i < 8; i++) 
 	{
-		for (var j = 0; j < 8; j++) 
+		for (var column = 0; column < 8; column++) 
 		{
-			buttonValueObj[i+1][j] = local.values.getChild("Main Buttons").getChild("button" + (i + 1) + (j + 1) + "");
-			colorParameterObj[i+1][j] = local.parameters.colors.getChild("Main Buttons").getChild("button" + (i + 1) + (j + 1) + "");
-			pulsingParameterObj[i+1][j] = local.parameters.pulsing.getChild("Main Buttons").getChild("button" + (i + 1) + (j + 1) + "");
+			buttonValueObj[i][column] = local.values.getChild("Main Buttons").getChild("button" + (i + 1) + (column + 1) + "");
+			colorParameterObj[i][column] = local.parameters.colors.getChild("Main Buttons").getChild("button" + (i + 1) + (column + 1) + "");
+			pulsingParameterObj[i][column] = local.parameters.pulsing.getChild("Main Buttons").getChild("button" + (i + 1) + (column + 1) + "");
+
+			var note = buttonNotes[i][column];
+			noteValueObj[note] = local.values.getChild("Main Buttons").getChild("button" + (i + 1) + (column + 1) + "");
+			noteColorObj[note] = local.parameters.colors.getChild("Main Buttons").getChild("button" + (i + 1) + (column + 1) + "");
+			notePulsingObj[note] = local.parameters.pulsing.getChild("Main Buttons").getChild("button" + (i + 1) + (column + 1) + "");
+
 		}
     }
 
 	// Side Buttons
     for (var i = 0; i < 8; i++) 
 	{
-        buttonValueObj[i+1][8] = local.values.getChild("Side Buttons").getChild("sideButton" + (i + 1));
-        colorParameterObj[i+1][8] = local.parameters.colors.getChild("Side Buttons").getChild("sideButton" + (i + 1));
-        pulsingParameterObj[i+1][8] = local.parameters.pulsing.getChild("Side Buttons").getChild("sideButton" + (i + 1));
+        buttonValueObj[i][8] = local.values.getChild("Side Buttons").getChild("sideButton" + (i + 1));
+        colorParameterObj[i][8] = local.parameters.colors.getChild("Side Buttons").getChild("sideButton" + (i + 1));
+
+		var note = sideButtonNotes[i];
+        noteValueObj[note] = local.values.getChild("Side Buttons").getChild("sideButton" + (i + 1));
+        noteColorObj[note] = local.parameters.colors.getChild("Side Buttons").getChild("sideButton" + (i + 1));
     }
 
-	// Setup Mode and Colors
-	setMidiMode();
-	resendColors();
+	// Bottom Buttons
+    for (var i = 0; i < 9; i++) 
+	{
+        buttonValueObj[8][i] = local.values.getChild("Bottom Buttons").getChild("bottomButton" + (i + 1) + "");
+
+		if (i < 8)
+        colorParameterObj[8][i] = local.parameters.colors.getChild("Bottom Buttons").getChild("bottomButton" + (i + 1) + "");
+
+		var note = bottomButtonNotes[i];
+        noteValueObj[note] = local.values.getChild("Bottom Buttons").getChild("bottomButton" + (i + 1) + "");
+
+		if (i < 8)
+			noteColorObj[note] = local.parameters.colors.getChild("Bottom Buttons").getChild("bottomButton" + (i + 1) + "");
+    }
+
+
+	if (local.parameters.isConnected) resync();
 }
 
 
+// ------ Chataigne Events ------
+
+var midiDeviceOutLast;
+var resyncReady = false;
+
+function moduleValueChanged(value) {
+}
+
+function moduleParameterChanged(param)
+{
+	script.log("Parameter Changed: " + param.name + " Parent: " + param.getParent().name + " GrandParent: " + param.getParent().getParent().name + " Value: " + param.get());
+
+	if (param.name == "devices") {
+		var midiOutDevice = param.get()[1];
+		if (param.get()[1] != midiDeviceOutLast) {
+			midiDeviceOutLast = midiOutDevice;
+
+			if (midiOutDevice) {
+				script.log("New Midi out Device detected");
+				resyncReady = true;
+				// resync();
+			}
+		}
+	}
+
+
+	// Color Feedback
+	if(param.getParent().getParent().name == "colors")
+	{
+		var color = param.get();
+
+		if(param.getParent().name == "mainButtons")
+		{
+			var id = [(parseInt(param.name.charAt(6))-1), (parseInt(param.name.charAt(7))-1)];
+			sendButtonColor(id, color);
+		}
+		else if(param.getParent().name == "sideButtons")
+		{
+			var index = parseInt(param.name.charAt(10)) - 1;
+			sendButton(true, index, param.get());
+		}
+		else if(param.getParent().name == "bottomButtons")
+		{
+			var index = (parseInt(param.name.charAt(12)) - 1);
+			script.log(index);
+			sendButton(false, index, param.get());
+		}
+	}
+
+	// Pulsing
+	if(param.getParent().getParent().name == "pulsing")
+	{
+		// if pulsing just got turned of send full brightness color again
+		if (param.get() == false){
+			if(param.getParent().name == "mainButtons")
+			{
+				// var id = [(parseInt(param.name.charAt(6))-1), (parseInt(param.name.charAt(7))-1)];
+				var id = [(parseInt(param.name.charAt(6))-1), (parseInt(param.name.charAt(7))-1)];
+				var color = colorParameterObj[id[0]][id[1]].get();
+				sendButtonColor(id, color);
+			}
+		}
+	}
+}
 
 function update(delta)
 {
+	if (!local.parameters.isConnected) return;
+
+	if (resyncReady)
+	{
+		resyncReady = false;
+		resync();
+	}
+
 	var time = util.getTime(); // time in seconds
 	var step = Math.round(((time % pulsingRate) / pulsingRate) * pulsingSteps);
 
 	if (step !== previousStep)
 	{
 		var intensity = pulsingTable[step];
-		script.log("Step: "+step+" intensity: "+intensity);
+		// script.log("Step: "+step+" intensity: "+intensity);
 
-		for (var i = 0; i < 9; i ++)
+		for (var i = 0; i < 8; i ++)
 		{
-			for (var j = 0; j < 9; j ++)
+			for (var j = 0; j < 8; j ++)
 			{
+				// script.log(i + " " + j);
 				if (pulsingParameterObj[i][j].get())
 				{
 					// scale color with intensity
 					var color = colorParameterObj[i][j].get();
-					// newColor[0] = Math.round((parseInt(color[0]) * intensity) / 255);
-					// script.log("R: "+ parseInt(color[0]));
-					// newColor[1] = Math.round((color[1] * intensity) / 255);
-					// newColor[2] = Math.round((color[2] * intensity) / 255);
-					setButtonColorBrightness([i,j], color, intensity);
+					sendButtonColorBrightness([i,j], color, intensity);
 				}
 			}
 		}
@@ -225,86 +299,13 @@ function update(delta)
 }
 
 
-
-function moduleParameterChanged(param)
-{
-	// Debugging 
-	script.log(param.name + " parameter changed, new value: " + param.get());
-
-	// Connection
-    if (param.name == "isConnected")
-	{
-        if (param.get() == 1)
-		{
-			util.delayThreadMS(3000);
-			setMidiMode();
-		} 
-		resendColors();
-    }
-
-
-	// Color Feedback
-	if(param.getParent().getParent().name == "colors")
-	{
-		var color = param.get();
-
-		if(param.name == "logo")
-		{
-			var id = [0, 8];
-			setButtonColor(id, color);
-		}
-		else if(param.getParent().name == "topButtons")
-		{
-			var id = [0, (parseInt(param.name.charAt(9))-1)];
-			setButtonColor(id, color);
-		}
-		else if(param.getParent().name == "mainButtons")
-		{
-			var id = [(parseInt(param.name.charAt(6))), (parseInt(param.name.charAt(7))-1)];
-			setButtonColor(id, color);
-		}
-		else if(param.getParent().name == "sideButtons")
-		{
-			var id = [parseInt(param.name.charAt(10)), 8];
-			setButtonColor(id, color);
-		}
-	}
-
-	// Pulsing
-	if(param.getParent().getParent().name == "pulsing")
-	{
-		// if pulsing just got turned of send pull brightness color again
-		if (!param.get()){
-			var color = param.get();
-
-			if(param.name == "logo")
-			{
-				var id = [0, 8];
-				setButtonColor(id, color);
-			}
-			else if(param.getParent().name == "topButtons")
-			{
-				var id = [0, (parseInt(param.name.charAt(9))-1)];
-				setButtonColor(id, color);
-			}
-			else if(param.getParent().name == "mainButtons")
-			{
-				var id = [(parseInt(param.name.charAt(6))), (parseInt(param.name.charAt(7))-1)];
-				setButtonColor(id, color);
-			}
-			else if(param.getParent().name == "sideButtons")
-			{
-				var id = [parseInt(param.name.charAt(10)), 8];
-				setButtonColor(id, color);
-			}
-		}
-	}
-}
-
+// ----- Midi Events ------
 
 function noteOnEvent(channel, note, velocity)
 {
-	var button = getButtonForNote(note);
+	if (channel != 1) return;
+
+	var button = noteValueObj[note];
 	if (!!button)
 	{
 		button.set((velocity==127));
@@ -314,7 +315,9 @@ function noteOnEvent(channel, note, velocity)
 
 function noteOffEvent(channel, note, velocity)
 {
-	var button = getButtonForNote(note);
+	if (channel != 1) return;
+
+	var button = noteValueObj[note];
 	if (!!button)
 	{
 		button.set(false);
@@ -324,7 +327,9 @@ function noteOffEvent(channel, note, velocity)
 
 function ccEvent(channel, note, value)
 {
-	var fader = getFaderForNote(note);
+	if (channel != 1) return;
+
+	var fader = ccValueObj[note];
 	if (!!fader)
 	{
 		fader.set(value/127);
@@ -335,4 +340,100 @@ function ccEvent(channel, note, value)
 function sysExEvent(data)
 {
 	script.log("Sysex Message received, "+data.length+" bytes :");
+}
+
+
+// ----- User Functions ------
+function setColorEnum(id, color)
+{
+	var colorObj = colorParameterObj[id[0]][id[1]];
+	if (!!colorObj)
+	{
+		colorObj.set(color);
+	}
+}
+
+function setColorByNote(note, color)
+{
+	var colorObj = noteColorObj[note];
+	if (!!colorObj)
+	{
+		colorObj.set(color);
+	}
+}
+
+function setColorByIndex(index, color)
+{
+	var x = Math.floor(index / 8);
+	var y = index % 8;
+	var colorObj = colorParameterObj[x][y];
+	if (!!colorObj)
+	{
+		colorObj.set(color);
+	}
+}
+
+function setColorByPosition(x, y, color)
+{
+	var colorObj = colorParameterObj[x-1][y-1];
+	if (!!colorObj)
+	{
+		colorObj.set(color);
+	}
+}
+
+function setPulsingEnum(id, pulsing)
+{
+	var pulsingObj = pulsingParameterObj[id[0]][id[1]];
+	if (!!pulsingObj)
+	{
+		pulsingObj.set(pulsing);
+	}
+}
+
+function setPulsingByNote(note, pulsing)
+{
+	var pulsingObj = notePulsingObj[note];
+	if (!!pulsingObj)
+	{
+		pulsingObj.set(pulsing);
+	}
+}
+
+function setPulsingByIndex(index, pulsing)
+{
+	var x = Math.floor(index / 8);
+	var y = index % 8;
+	var pulsingObj = pulsingParameterObj[x][y];
+	if (!!pulsingObj)
+	{
+		pulsingObj.set(pulsing);
+	}
+}
+
+function setPulsingByPosition(x, y, pulsing)
+{
+	var pulsingObj = pulsingParameterObj[x-1][y-1];
+	if (!!pulsingObj)
+	{
+		pulsingObj.set(pulsing);
+	}
+}
+
+function setButtonLed(isBottom, index, state)
+{
+	var obj = isBottom ? colorParameterObj[8][index-1] : colorParameterObj[index-1][8];
+	if (!!obj)
+	{
+		obj.set(state);
+	}
+}
+
+function setButtonLedByNote(note, state)
+{
+	var obj = noteColorObj[note];
+	if (!!obj)
+	{
+		obj.set(state);
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # APC-Mini-mkII-Chataigne-Module
 A Chataigne module for the Akai APC-Mini-mkII MIDI Controller
+
+## Important Notes
+In order to make the module work connect Midi in and out to `APC mini mk2 Control`.
+
+## Features
+- All inputs mapped
+- Full RGB 14bit color feedback
+- Last state automatically restored on reconnect
+- Extensive user commands
